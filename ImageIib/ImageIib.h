5fd849edf08fa94a9573dda8597796935788071d
Danish <danish.aziz@photobookworldwide.com>
2018-12-21 20:18:52 +0800

    Initial commit

---
+++ ImageIib/ImageIib.h
@@ -0,0 +1,19 @@
+//
+//  ImageIib.h
+//  ImageIib
+//
+//  Created by Danish on 21/12/2018.
+//  Copyright © 2018 Danish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageIib.
+FOUNDATION_EXPORT double ImageIibVersionNumber;
+
+//! Project version string for ImageIib.
+FOUNDATION_EXPORT const unsigned char ImageIibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageIib/PublicHeader.h>
+
+
